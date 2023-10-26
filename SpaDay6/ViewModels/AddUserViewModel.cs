7cd17f178887be12253c748993f1f6612b6910fc
Jada <jada.hundley@health.slu.edu>
2023-10-25 19:51:52 -0500

    Created new viewmodels and added validation attributes

---
+++ SpaDay6/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpaDay6.ViewModels
+{
+	public class AddUserViewModel
+	{
+		[Required(ErrorMessage = "Username is required")]
+		[StringLength(15, MinimumLength = 5, ErrorMessage = "Username must be between 5 and 15 characters long")]
+		public string? Username { get; set; }
+
+		[Required(ErrorMessage = "Password is required")]
+		[StringLength(20, MinimumLength = 6, ErrorMessage = "Sorry, the given password is too short. It must be at least 6 characters long")]
+		public string? Password { get; set; }
+
+		[Required(ErrorMessage = "Password doesn't match")]
+		[StringLength(20, MinimumLength = 6)]
+		public string? VerifyPassword { get; set; }
+
+		[EmailAddress]
+        public string? Email { get; set; }
+    }
+}
+
